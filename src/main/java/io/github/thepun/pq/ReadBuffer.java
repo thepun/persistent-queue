f0de7a0f781b36ca340e078e09732dc1a66ae813
Marat Gariev <thepun599@gmail.com>
2018-06-05 15:44:21 +0300

    buffers started

--- src/main/java/io/github/thepun/pq/ReadBuffer.java
+++ src/main/java/io/github/thepun/pq/ReadBuffer.java
@@ -1,4 +1,16 @@
 package io.github.thepun.pq;
 
+import io.github.thepun.unsafe.chars.OffHeapCharSequence;
+
 public interface ReadBuffer {
+
+    byte readByte();
+    char readChar();
+    short readShort();
+    int readInt();
+    long readLong();
+    boolean readBoolean();
+    void readOffHeap(long address, int length);
+    void readOffHeap(OffHeapCharSequence offHeapCharSequence);
+
 }
--- src/main/java/io/github/thepun/pq/WriteBuffer.java
+++ src/main/java/io/github/thepun/pq/WriteBuffer.java
@@ -4,9 +4,11 @@ import io.github.thepun.unsafe.chars.OffHeapCharSequence;
 
 public interface WriteBuffer {
 
+    void writeByte(byte value);
+    void writeChar(char value);
+    void writeShort(short value);
     void writeInt(int value);
     void writeLong(long value);
-    void writeString(String value);
     void writeBoolean(boolean value);
     void writeOffHeap(long address, int length);
     void writeOffHeap(OffHeapCharSequence offHeapCharSequence);
