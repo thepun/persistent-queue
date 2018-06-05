a1a0533dc2d88edbf98b8cedd62775286fc2d7c3
Marat Gariev <thepun599@gmail.com>
2018-06-05 17:20:36 +0300

    read/write buffers

--- src/main/java/io/github/thepun/pq/DataReader.java
+++ src/main/java/io/github/thepun/pq/DataReader.java
@@ -1,58 +1,110 @@
 package io.github.thepun.pq;
 
+import io.github.thepun.unsafe.OffHeapMemory;
 import io.github.thepun.unsafe.chars.OffHeapCharSequence;
+
+import java.nio.MappedByteBuffer;
 
 final class DataReader implements ReadBuffer {
 
+    private final MappedByteBuffer buffer;
+
+    private long cursor;
+    private long limit;
+
     DataReader(Data data) {
-
-    }
-
-    void setCursor(long cursor) {
-
-    }
-
-    void setLimit(long limit) {
-
+        buffer = data.getBuffer();
     }
 
     @Override
     public byte readByte() {
-        return 0;
+        if (cursor == limit) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        byte value = buffer.get((int)cursor);
+        cursor++;
+        return value;
     }
 
     @Override
     public char readChar() {
-        return 0;
+        if (cursor + 2 >= limit) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        char value = buffer.getChar((int)cursor);
+        cursor += 2;
+        return value;
     }
 
     @Override
     public short readShort() {
-        return 0;
+        if (cursor + 2 >= limit) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        short value = buffer.getShort((int)cursor);
+        cursor += 2;
+        return value;
     }
 
     @Override
     public int readInt() {
-        return 0;
+        if (cursor + 4 >= limit) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        int value = buffer.getInt((int)cursor);
+        cursor += 4;
+        return value;
     }
 
     @Override
     public long readLong() {
-        return 0;
+        if (cursor + 8 >= limit) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        long value = buffer.getInt((int)cursor);
+        cursor += 8;
+        return value;
     }
 
     @Override
     public boolean readBoolean() {
-        return false;
+        if (cursor == limit) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        byte value = buffer.get((int)cursor);
+        cursor++;
+        return value == 1;
     }
 
     @Override
     public void readOffHeap(long address, int length) {
+        if (cursor + length >= limit) {
+            throw new IndexOutOfBoundsException();
+        }
 
+        for (int i = 0; i < length; i++) {
+            OffHeapMemory.setByte(address + i, buffer.get((int)(cursor + i)));
+        }
+
+        cursor += length;
     }
 
     @Override
     public void readOffHeap(OffHeapCharSequence offHeapCharSequence) {
+        readOffHeap(offHeapCharSequence.getOffheapAddress(), offHeapCharSequence.getOffheapLength());
+    }
 
+    void setCursor(long cursor) {
+        this.cursor = cursor;
+    }
+
+    void setLimit(long limit) {
+        this.limit = limit;
     }
 }
--- src/main/java/io/github/thepun/pq/DataWriter.java
+++ src/main/java/io/github/thepun/pq/DataWriter.java
@@ -1,67 +1,79 @@
 package io.github.thepun.pq;
 
+import io.github.thepun.unsafe.OffHeapMemory;
 import io.github.thepun.unsafe.chars.OffHeapCharSequence;
 
 import java.nio.MappedByteBuffer;
 
 final class DataWriter implements WriteBuffer {
 
-    private final int size;
     private final MappedByteBuffer buffer;
 
     private long cursor;
 
     DataWriter(Data data) {
         buffer = data.getBuffer();
-        size = buffer.capacity();
     }
 
     @Override
     public void writeByte(byte value) {
-
+        buffer.put((int) cursor, value);
+        cursor++;
     }
 
     @Override
     public void writeChar(char value) {
-
+        buffer.putChar((int) cursor, value);
+        cursor += 2;
     }
 
     @Override
     public void writeShort(short value) {
-
+        buffer.putShort((int) cursor, value);
+        cursor += 2;
     }
 
     @Override
     public void writeInt(int value) {
-
+        buffer.putInt((int) cursor, value);
+        cursor += 4;
     }
 
     @Override
     public void writeLong(long value) {
-
+        buffer.putLong((int) cursor, value);
+        cursor += 8;
     }
 
     @Override
     public void writeBoolean(boolean value) {
-
+        buffer.put((int) cursor, value ? (byte) 1 : (byte) 0);
+        cursor++;
     }
 
     @Override
     public void writeOffHeap(long address, int length) {
-
+        for (int i = 0; i < length; i++) {
+            buffer.put((int) (cursor + i), OffHeapMemory.getByte(address + i));
+        }
+        cursor += length;
     }
 
     @Override
     public void writeOffHeap(OffHeapCharSequence offHeapCharSequence) {
-
+        writeOffHeap(offHeapCharSequence.getOffheapAddress(), offHeapCharSequence.getOffheapLength());
     }
 
     void mark(long sequenceId) {
-
+        align();
+        buffer.putLong((int) cursor, sequenceId);
+        cursor += 8;
     }
 
     void commit(long sequenceId) {
-
+        align();
+        buffer.putLong((int) cursor, sequenceId);
+        cursor += 8;
     }
 
     void setCursor(long cursor) {
@@ -70,5 +82,11 @@ final class DataWriter implements WriteBuffer {
 
     long getCursor() {
         return cursor;
+    }
+
+    private void align() {
+        if (cursor % 8 != 0) {
+            cursor = cursor / 8 * 8 + 8;
+        }
     }
 }
